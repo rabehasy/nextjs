036df3255d8209040642da9adfc598553e47e659
Miary <miary.rabehasy@lecolefrancaise.fr>
2021-12-24 23:03:45 +0100

    Nextjs

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+import Layout, { siteTitle } from '../components/layout'
+
+export default function Custom404() {
+    return (
+        <Layout> 404 Not found  </Layout>
+    )
+}
---
+++ pages/contact.js
@@ -0,0 +1,7 @@
+import Layout, { siteTitle } from '../components/layout'
+
+export default function Contact() {
+    return (
+        <Layout> Contact </Layout>
+    )
+}
